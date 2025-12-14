39c1be3e7d14cbeb2387a69b2da12cae9c01b81e
Felix <hongphuc292000@gmail.com>
2025-12-14 11:49:10 +0700

    fix type issue when build

--- src/util/sanity.ts
+++ src/util/sanity.ts
@@ -1,9 +1,8 @@
 import { createImageUrlBuilder } from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "@/sanity/client";
 
 const { projectId, dataset } = client.config();
-export const urlFor = (source: SanityImageSource) =>
+export const urlFor = (source: any) =>
   projectId && dataset
     ? createImageUrlBuilder({ projectId, dataset }).image(source)
     : null;
